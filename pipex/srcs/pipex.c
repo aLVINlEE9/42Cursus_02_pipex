99113387dd440ab0b4a3504033d6f2d25d5b517d
aLVINlEE9 <fldh3746@gmail.com>
2021-09-06 15:22:17 +0900

    pipex

--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: seungsle <seungsle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 18:08:38 by seungsle          #+#    #+#             */
-/*   Updated: 2021/09/06 15:01:16 by seungsle         ###   ########.fr       */
+/*   Updated: 2021/09/06 15:22:08 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 
 int	find_path(char *cmd, char **evnp)
 {
+	char **paths;
+	char *path_push;
+	char *path;
+
 	while (!strnstr(envp[i], "PATH"))
 		i++;
-	
+	paths = ft_split(envp[i] + 5, ':');
+	while (paths[i++])
+	{
+		path_push = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(path_push, cmd);
+		if (access(path, F_OK))
+			return path;
+	}
+	return (0);
 }
 
 int	execute(char *cmd, char **envp)
@@ -33,11 +45,11 @@ int cmd_child(int *fd_pipe, char *cmd, char **envp)
 	return (0);
 }
 
-int	cmd_parent(int *fd_pipe, char *cmd)
+int	cmd_parent(int *fd_pipe, char *cmd, char **envp)
 {
 	dup2(fd_pipe[0], STDIN_FILENO);
 	close(fd_pipe[1]);
-	execute(cmd);
+	execute(cmd, envp);
 	return (0);
 }
 
@@ -75,7 +87,7 @@ int main(int argc, char **argv, char **envp)
 	waitpid(pid, NULL, 0);
 	printf("parent process\n");
 	redirect_out(argv);
-	cmd_parent(fd_pipe, argv[3]);
+	cmd_parent(fd_pipe, argv[3], envp);
 	close(fd_pipe[0]);
 	close(fd_pipe[1]);
 }
