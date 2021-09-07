ba4892298d915bee80150e8a3cbdd4c00361d48f
aLVINlEE9 <fldh3746@gmail.com>
2021-09-07 16:18:02 +0900

    pipex modified

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: seungsle <seungsle@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 18:15:53 by seungsle          #+#    #+#             */
-/*   Updated: 2021/09/06 18:57:56 by seungsle         ###   ########.fr       */
+/*   Updated: 2021/09/07 16:17:36 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ int		redirect_out(char *argv, int *fd_pipe);
 char	*get_cmd_path(char *argv, char **envp);
 char	**get_cmd(char *argv);
 void	error(char **cmd, char *path, char *str, int f);
-void  free_util(char **cmd);
+void	free_util(char **cmd);
 
 #endif
